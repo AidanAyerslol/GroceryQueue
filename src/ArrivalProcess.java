44e9a74caed712dcb1e514244cc7b3bd06248d8f
ayushrg <ayushregmi736@gmail.com>
2025-03-05 11:11:44 -0500

    added a constructor in Arrival Process, initialized the main function

--- src/ArrivalProcess.java
+++ src/ArrivalProcess.java
@@ -1,7 +1,12 @@
 public class ArrivalProcess {
     int jobcounter;
-    final double arrivalrate = 0.15; //Can change throughout the day, may be implemented in traffic project
+    double arrivalrate; //Can change throughout the day, may be implemented in traffic project
     ExponentialDistribution exponentialdistribution = new ExponentialDistribution(arrivalrate);
+
+    // constructor
+    ArrivalProcess(double arrivalrate) {
+        this.arrivalrate = arrivalrate;
+    }
 
     public double getNextArrivalTime() { return exponentialdistribution.sample(); }
     public Job getNextJob(double currentTime) { return new Job(currentTime, jobcounter++); }
--- src/main.java
+++ src/main.java
@@ -1,3 +1,11 @@
 public class main {
+    // setting simulation parameters
+    final double averageShoppingRate = 0.1; // assume average shopping time 10 minutes
+    final double arrivalRate = 0.15; //  assume average arrival time 15 jobs per minute
+    ExponentialDistribution exponentialDistribution = new ExponentialDistribution(averageShoppingRate);
+
+    int simulationEndTime = 560; // 9 hours * 60 minutes (we can change it later)
+    double shoppingTimeDistribution = exponentialDistribution.sample();
+
 
 }
